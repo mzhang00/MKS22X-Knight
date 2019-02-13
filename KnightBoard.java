bb13101063712b1f6175e651532d39cd7f839f55
Michael Zhang <mzhang00@stuy.edu>
2019-02-13 09:48:28 -0500

    Add constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,3 +1,16 @@
 public class KnightBoard{
   int[][] data;
+
+  public KnightBoard(int startingRows,int startingCols){
+    if (startingCols < 0 || startingRows < 0){
+      throw new IllegalArgumentException();
+    }
+    data = new int[startingRows][startingCols];
+    for (int r = 0; r < startingRows; r++){
+      for (int c = 0; c < startingCols; c++){
+        data[r][c] = 0;
+      }
+    }
+  }
+
 }
