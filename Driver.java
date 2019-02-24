61e66bd215e7ee207b471b78fcfec0b8f87ec216
Michael Zhang <mzhang00@stuy.edu>
2019-02-24 00:25:12 -0500

    Update driver to test countSolutions

--- Driver.java
+++ Driver.java
@@ -1,6 +1,16 @@
 public class Driver{
   public static void main(String[] args){
-    KnightBoard k = new KnightBoard(5, 5);
-    System.out.println(k);
+    KnightBoard k; 
+    System.out.println("-------------------------------------------------");
+    //System.out.println(k);
+    for (int i = 1; i <= 6; i++){
+      for (int c = i; c <= 6; c++){
+        System.out.println("" + i + "x" + c);
+        System.out.println();
+        k = new KnightBoard(i, c);
+        System.out.println(k.countSolutions(0, 0));
+        System.out.println("-------------------------------------------------");
+      }
+    }
   }
 }
