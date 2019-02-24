b9a2f060417b6b6f068ea053bd1e5b6a40ec133d
Michael Zhang <mzhang00@stuy.edu>
2019-02-24 00:58:56 -0500

    Update solve helper

--- Driver.java
+++ Driver.java
@@ -3,8 +3,8 @@ public class Driver{
     KnightBoard k; 
     System.out.println("-------------------------------------------------");
     //System.out.println(k);
-    for (int i = 1; i <= 6; i++){
-      for (int c = i; c <= 6; c++){
+    for (int i = 1; i <= 4; i++){
+      for (int c = i; c <= 4; c++){
         System.out.println("" + i + "x" + c);
         System.out.println();
         k = new KnightBoard(i, c);
--- KnightBoard.java
+++ KnightBoard.java
@@ -59,11 +59,14 @@ public class KnightBoard{
     if (startingCol < 0 || startingRow < 0 || startingCol >= data[0].length || startingRow >= data.length){
       throw new IllegalArgumentException();
     }
-    return solveH(startingRow, startingCol, 0);
+    return solveH(startingRow, startingCol, 1);
   }
 
   private boolean solveH(int row ,int col, int moveNumber){
-    return true;
+    if (moveNumber == data.length * data[0].length){
+      return true;
+    }
+    return false;
   }
 
   public int countSolutions(int startingRow, int startingCol){
