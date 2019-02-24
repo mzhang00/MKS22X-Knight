b84dd9c61784149c4b565e3187d4c1bdcee7ffdb
Michael Zhang <mzhang00@stuy.edu>
2019-02-24 01:26:36 -0500

    Test solve with more cases

--- Driver.java
+++ Driver.java
@@ -2,8 +2,8 @@ public class Driver{
   public static void main(String[] args){
     KnightBoard k; 
     System.out.println("-------------------------------------------------");
-    for (int i = 1; i <= 4; i++){
-      for (int c = i; c <= 4; c++){
+    for (int i = 1; i <= 7; i++){
+      for (int c = i; c <= 7; c++){
         System.out.println("" + i + "x" + c);
         System.out.println();
         k = new KnightBoard(i, c);
